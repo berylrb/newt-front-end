3af4d6c599908b023761071d938d4aeba23a3a26
Rachel Kinzel <rharen11@su.edu>
2022-07-15 11:52:00 -0700

    refactor user activity to use profile service

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ChangePassword from './pages/ChangePassword/ChangePassword'
 import * as authService from './services/authService'
 import Search from './pages/Search/Search'
 import ActivityDetails from './components/ActivityDetails/ActivityDetails'
-import * as userActivityService from './services/userActivityService'
+import * as profileService from './services/profileService'
 
 
 
@@ -33,13 +33,13 @@ const App = () => {
   }
 
   const handleAddUserActivity = async newUserActivityData => {
-    const newUserActivity = await userActivityService.create(newUserActivityData)
+    const newUserActivity = await profileService.create(newUserActivityData)
     setUserActivity([...userActivity, newUserActivity])
   }
 
   useEffect(() => {
     const fetchAllUserActivities = async () =>{
-      const userActivityData = await userActivityService.getAll()
+    const userActivityData = await profileService.getAll()
       setUserActivity(userActivityData)
     }
     fetchAllUserActivities()
