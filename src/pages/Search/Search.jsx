daf020052b6c6fbf1b9a64e3256fdfe6f3e76bfb
Kevin <kevin.rodriguez678@gmail.com>
2022-07-15 11:06:12 -0400

    add dropdown menu along with labels to search form

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,4 +1,4 @@
-
+import SearchForm from "../../components/SearchForm/SearchForm"
 
 const Search = () => {
   return (
@@ -6,6 +6,7 @@ const Search = () => {
       <div className="landing-bg">
         <main className="search">
           <h1>Search page</h1>
+          <SearchForm />
         </main>
       </div>
     </>
