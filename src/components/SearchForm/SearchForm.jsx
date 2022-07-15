ae8c7599714808ae0f546245a4633d4542487cb6
Kevin <kevin.rodriguez678@gmail.com>
2022-07-15 10:38:48 -0400

    fix error

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,3 +1,5 @@
+
+
 const SearchForm = () => {
   return (
     <form
@@ -7,7 +9,19 @@ const SearchForm = () => {
     >
       <div>
         <label></label>
-        
+        <select name="" id="">
+          <option value="0.0">0</option>
+          <option value="0.1">1</option>
+          <option value="0.2">2</option>
+          <option value="0.3">3</option>
+          <option value="0.4">4</option>
+          <option value="0.5">5</option>
+          <option value="0.6">6</option>
+          <option value="0.7">7</option>
+          <option value="0.8">8</option>
+          <option value="0.9">9</option>
+          <option value="1.0">10</option>
+        </select>
       </div>
     </form>
   );
