bd7c245ee75254fd7f3e30ade40ba7df4161db57
Rachel Kinzel <rharen11@su.edu>
2022-07-15 10:11:54 -0700

    add create fxn to userActivity service module

---
+++ src/services/userActivityService.js
@@ -0,0 +1,19 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/userActivities`
+
+async function create(userActivity) {
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization' : `Bearer ${tokenService.getToken()}`
+    },
+    body: JSON.stringify(userActivity)
+  })
+  return res.json()
+}
+
+export {
+  create
+}
+
