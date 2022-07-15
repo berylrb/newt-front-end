baec7d7ee989c78ffef6fde132f73ebb7384d11b
Kevin <kevin.rodriguez678@gmail.com>
2022-07-15 10:30:13 -0400

    work on search form

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,16 @@
+const SearchForm = () => {
+  return (
+    <form
+      autoComplete="off"
+      className="search"
+      method="POST"
+    >
+      <div>
+        <label></label>
+        
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
