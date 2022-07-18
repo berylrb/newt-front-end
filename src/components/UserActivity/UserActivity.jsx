8acbfee0d054b204bb92802750df96d2d6bbdb31
Beryl B <beriebaldwin@gmail.com>
2022-07-18 10:47:27 -0400

    css for user activity form

--- src/components/UserActivity/UserActivity.jsx
+++ src/components/UserActivity/UserActivity.jsx
@@ -1,16 +1,19 @@
+import styles from './UserActivity.module.css'
 
 const UserActivity = (props) => {
   console.log(props)
   return (
 
-    <div>
-      <ul>
-        <li>{props.activity.activity}</li>
-        <li>{props.activity.type}</li>
-        <li>${props.activity.price}</li>
-        <li>How many peeps: {props.activity.participants}</li>
-        <button onClick={() => props.handleDeleteUserActivity(props.activity._id)}>Delete</button>
-      </ul>
+    <div className={styles.userActivityDiv}>
+        <h4>Activity:</h4>
+        <p>{props.activity.activity}</p>
+        <h4>Type:</h4>
+        <p>{props.activity.type}</p>
+        <h4>Price:</h4>
+        <p>${props.activity.price}</p>
+        <h4>Participants:</h4>
+        <p>{props.activity.participants}</p>
+        <button className={styles.button6} onClick={() => props.handleDeleteUserActivity(props.activity._id)}>Delete</button>
     </div>
   )
 }
