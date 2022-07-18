07c92c9dc587eb93422d57b682aac66f3c121441
Beryl B <beriebaldwin@gmail.com>
2022-07-18 15:03:21 -0400

    fix log in

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,43 +1,29 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import styles from './LoginForm.module.css'
 import * as authService from '../../services/authService'
 
 const LoginForm = props => {
-  const navigate = useNavigate()
   const [formData, setFormData] = useState({
-    name: '',
     email: '',
     pw: '',
-    pwConf: '',
   })
-
-  const [photoData, setPhotoData] = useState({})
+  const navigate = useNavigate()
 
   const handleChange = e => {
     props.updateMessage('')
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
-  const handleChangePhoto = (evt) => {
-    setPhotoData({ photo: evt.target.files[0] })
-  }
-
   const handleSubmit = async evt => {
     evt.preventDefault()
     try {
-      await authService.signup(formData, photoData.photo)
+      await authService.login(formData)
       props.handleSignupOrLogin()
       navigate('/')
     } catch (err) {
       props.updateMessage(err.message)
     }
-  }
-
-  const { name, email, pw, pwConf } = formData
-
-  const isValid = () => {
-    return !(name && email && pw && pw === pwConf)
   }
 
   return (
@@ -46,17 +32,6 @@ const LoginForm = props => {
       onSubmit={handleSubmit}
       className={styles.container}
     >
-      <div className={styles.inputContainer}>
-        <label htmlFor="name" className={styles.label}>Name</label>
-        <input
-          type="text"
-          autoComplete="off"
-          id="name"
-          value={formData.name}
-          name="name"
-          onChange={handleChange}
-        />
-      </div>
       <div className={styles.inputContainer}>
         <label htmlFor="email" className={styles.label}>Email</label>
         <input
@@ -78,26 +53,6 @@ const LoginForm = props => {
           name="pw"
           onChange={handleChange}
         />
-      </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="confirm" className={styles.label}>Confirm Password</label>
-        <input
-          type="text"
-          autoComplete="off"
-          id="confirm"
-          value={formData.pwConf}
-          name="pwConf"
-          onChange={handleChange}
-        />
-        <div className={styles.inputContainer}>
-        <label htmlFor="photo-upload" className={styles.label}>Upload Photo</label>
-        <input
-          type="file"
-          id="photo-upload"
-          name="photo"
-          onChange={handleChangePhoto}
-        />
-      </div>
       </div>
       <div className={styles.buttonDiv}>
         <button className={styles.button6}>Log In</button>
