9ec6dc11ebc62376ac4da822279ae12203e9e514
Beryl B <beriebaldwin@gmail.com>
2022-07-20 14:05:16 -0400

    trouble shoot api import

--- src/services/bored-api.js
+++ src/services/bored-api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://www.boredapi.com/api"
+const baseUrl = "https://www.boredapi.com/api"
 
 export async function getSoloActivity() {
   const res = await fetch(`${baseUrl}/activity?participants=1`)
