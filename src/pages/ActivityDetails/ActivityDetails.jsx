e71893ab56a29a171d5b2e57f7616a5e1f6f11d6
Beryl B <beriebaldwin@gmail.com>
2022-07-21 21:37:56 -0400

    table

--- src/pages/ActivityDetails/ActivityDetails.jsx
+++ src/pages/ActivityDetails/ActivityDetails.jsx
@@ -1,22 +1,22 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { getActivityDetails } from '../../services/bored-api'
 import { useLocation, useNavigate } from 'react-router-dom'
-import  styles from './ActivityDetails.module.css'
+import styles from './ActivityDetails.module.css'
 import * as profileService from '../../services/profileService'
 import * as activityService from '../../services/activityService'
 import CommentSection from '../../components/CommentSection/CommentSection'
 
 
-const ActivityDetails = ( {user} ) => {
+const ActivityDetails = ({ user }) => {
   const [activityDetails, setActivityDetails] = useState({})
   const location = useLocation()
   const [savedActivity, setSavedActivity] = useState()
   // const [show, setShow] = useState(false)
   const navigate = useNavigate()
   console.log(location)
-  const key = location.state.soloActivity 
-    ? location.state.soloActivity.key 
+  const key = location.state.soloActivity
+    ? location.state.soloActivity.key
     : location.state.groupActivity.key
 
   console.log("this is the key", key)
@@ -62,75 +62,77 @@ const ActivityDetails = ( {user} ) => {
     <>
       <div className={styles.detailsPage}>
         <div className={styles.allDetailsContents}>
-      {activityDetails.activity ?
-      <>
-        <div className={styles.activityDetails}>
-          <div className={styles.detailsHeader}>
-            <h4 className={styles.detailsH4}>ACTIVITY DETAILS</h4>
-          </div>
-          <table className={styles.table}>
-            <tbody>
-            <tr>
-              <th className={styles.th}>ACTIVITY</th>
-              <th className={styles.th}>TYPE</th>
-              <th className={styles.th}>PRICE</th>
-            </tr>
-            <tr>
-              <td className={styles.td}>{activityDetails.activity}</td>
-              <td className={styles.td}>{activityDetails.type}</td>
-              <td className={styles.td}>{activityDetails.price}</td>
-            </tr>
-            </tbody>
-          </table>
-          
-            <div className={styles.CommentSectionDiv}>
-              
-            </div>
-            <div className={styles.returnContainer}>
-              <br />
-              {savedActivity >= 0  ?
-              <>
-                <p className={styles.pSave}>Wanna save this activity? Add it to your list!</p>
-                <button onClick={handleSubmit} className={styles.addButton}>Add to List</button>
-              </>
-              :
-              <>
-                <p className={styles.pSave}> </p>
-              </>
-              }
-              <button className={styles.button6} onClick={buttonSubmit}>Go Back</button>
-            </div>
-            
-          </div>
-      </>
-      :
-      <>
-        <div className={styles.activityDetails}>
-        <div className={styles.detailsHeader}>
-            <h4 className={styles.detailsH4}>ACTIVITY DETAILS</h4>
-          </div>
-          <p>Loading activity details...</p>
-          <div className='returnContainer'>
-            <Link className='returnLink' to="/">Return</Link>
-          </div>
-        </div>
-      </>
-      }
+          {activityDetails.activity ?
+            <>
+              <div className={styles.activityDetails}>
+                <div className={styles.detailsHeader}>
+                  <h4 className={styles.detailsH4}>ACTIVITY DETAILS</h4>
+                </div>
+                <table className={styles.table}>
+                  <thead>
+                    <tr>
+                      <th className={styles.th}>ACTIVITY</th>
+                      <th className={styles.th}>TYPE</th>
+                      <th className={styles.th}>PRICE</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className={styles.td}>{activityDetails.activity}</td>
+                      <td className={styles.td}>{activityDetails.type}</td>
+                      <td className={styles.td}>{activityDetails.price}</td>
+                    </tr>
+                  </tbody>
+                </table>
 
-        {savedActivity &&
-        <div className={styles.activityDetailsBottom}>
-          <div className={styles.detailsHeaderBottom}>
-            <h4 className={styles.detailsH4Bottom}>COMMUNITY COMMENTS</h4>
-          </div>
-          <div className={styles.commentHolder}>
-                  <CommentSection 
-                    savedActivity={savedActivity}
-                    setSavedActivity={setSavedActivity}
-                    profile={user?.profile}
-                  />
+                <div className={styles.CommentSectionDiv}>
+
+                </div>
+                <div className={styles.returnContainer}>
+                  <br />
+                  {savedActivity >= 0 ?
+                    <>
+                      <p className={styles.pSave}>Wanna save this activity? Add it to your list!</p>
+                      <button onClick={handleSubmit} className={styles.addButton}>Add to List</button>
+                    </>
+                    :
+                    <>
+                      <p className={styles.pSave}> </p>
+                    </>
+                  }
+                  <button className={styles.button6} onClick={buttonSubmit}>Go Back</button>
+                </div>
+
+              </div>
+            </>
+            :
+            <>
+              <div className={styles.activityDetails}>
+                <div className={styles.detailsHeader}>
+                  <h4 className={styles.detailsH4}>ACTIVITY DETAILS</h4>
+                </div>
+                <p>Loading activity details...</p>
+                <div className='returnContainer'>
+                  <Link className='returnLink' to="/">Return</Link>
+                </div>
+              </div>
+            </>
+          }
+
+          {savedActivity &&
+            <div className={styles.activityDetailsBottom}>
+              <div className={styles.detailsHeaderBottom}>
+                <h4 className={styles.detailsH4Bottom}>COMMUNITY COMMENTS</h4>
+              </div>
+              <div className={styles.commentHolder}>
+                <CommentSection
+                  savedActivity={savedActivity}
+                  setSavedActivity={setSavedActivity}
+                  profile={user?.profile}
+                />
+              </div>
             </div>
-          </div>
-        }
+          }
         </div>
       </div>
     </>
